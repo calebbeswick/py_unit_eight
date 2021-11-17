c908fcdf06c360600903a8f930cf7712df8482f7
calebbeswick <beswica@ssfs.org>
2021-11-17 08:56:33 -0500

    Worked on Widgets

---
+++ tip_calculator.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Tip Calculator")
+
+
+# Variables that I will need
+cost = tk.StringVar()
+cost.set(0.0)
+
+word = tk.Label(root, text="Tip Calculator")
+word.grid(row=1, column=1)
+meal_cost_label = tk.Label(root, text="Meal Cost:")
+meal_cost_label.grid(row=2, column=1)
+
+meal_cost_entry = tk.Entry(root, textvariable=cost)
+meal_cost_entry.grid(row=2, column=2)
+
+
+
+
+root.mainloop()
